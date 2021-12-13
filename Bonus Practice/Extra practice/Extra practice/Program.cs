cf8fe6159954f8066edfd32db4c8af7ea0ae2221
Jarom Magalei <jarom.pro.magalei@gmail.com>
2021-12-12 18:29:19 -0700

    Finished up the heating up sections

--- Bonus Practice/Extra practice/Extra practice/Program.cs
+++ Bonus Practice/Extra practice/Extra practice/Program.cs
@@ -8,18 +8,19 @@ namespace Extra_practice
         {
             Print1000();
             By3();
-            Equal(4,5);
+            Equal(4, 5);
             EvenOrOdd(6);
             PosiOrNO(0);
             CanVote();
+            IsInRange();
+            MultiTab(5);
 
 
-            
         }
 
         public static void Print1000()
         {
-            for (int i = 1000; i >= -1000 ; i--)
+            for (int i = 1000; i >= -1000; i--)
             {
                 Console.WriteLine(i);
             }
@@ -27,7 +28,7 @@ namespace Extra_practice
 
         public static void By3()
         {
-            for (int i = 3; i <= 999; i+=3)
+            for (int i = 3; i <= 999; i += 3)
             {
                 Console.WriteLine(i);
             }
@@ -63,7 +64,7 @@ namespace Extra_practice
 
         public static void CanVote()
         {
-            
+
             bool key = true;
             while (key)
 
@@ -77,16 +78,57 @@ namespace Extra_practice
                     key = false;
                 }
                 else
-                { 
+                {
                     Console.WriteLine("You cannot vote");
                     key = true;
                 }
-                
+
             }
-           
-            
+
 
         }
 
+
+        public static void IsInRange()
+        {
+            bool key = true;
+            while (key)
+
+            {
+                Console.WriteLine("Input a number");
+                int num1 = int.Parse(Console.ReadLine());
+
+                if (num1 <= 10 && num1 >= -10)
+                {
+                    Console.WriteLine("It's in our range!");
+                    key = false;
+                }
+                else if (num1 < -10)
+                {
+                    Console.WriteLine("Oh darn, not in our range! Try a little higher.");
+                    key = true;
+                }
+                else if (num1 > 10)
+                {
+                    Console.WriteLine("Oh darn, not in our range... Try a little lower.");
+                    key = true;
+                }
+
+
+            }
+
+
+
+        }
+
+        public static void MultiTab(int num)
+        {
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{num} * {i} = {num * i}");
+            }
+        }
+    
     }
-}
+
+}              
